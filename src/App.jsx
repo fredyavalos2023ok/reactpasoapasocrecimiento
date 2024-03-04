3c0f90e16f3a40d8d651c5d79901adc2c416d554
afredy4 <afredy.ronaldo4@gmail.com>
2024-03-04 09:12:10 -0500

    ya agregué el componente a la raiz    App.jsx y pasé sus props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
 export function App () {
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img
-                className='tw-followCard-avatar' 
-                alt="El avatar de midudev" 
-                src="https://unavatar.io/midudev" />
-                <div className="tw-followCard-info">
-                    <strong>Miguel Ángel Durán</strong>
-                    <span 
-                    className="tw-followCard-infoUserName">@midudev</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className="tw-followCard-button">
-                    Seguir
-                </button>
-            </aside>
-        </article>
+    <>
+        <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" />
+        <TwitterFollowCard userName="midudev" name="pheralb" />
+        <TwitterFollowCard userName="midudev" name="elon musk" />
+        <TwitterFollowCard userName="midudev" name="vxnder" />
+    </>
     )
 }
